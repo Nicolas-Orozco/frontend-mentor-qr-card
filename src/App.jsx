23fefd10ad45e396d6d23975871c8142b6b4fd6c
Nicolas Orozco Davila <nico.orozco.davila@yandex.com>
2022-05-14 21:36:29 -0500

    Add main text

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/Header";
+import Main from "./components/Main";
 
 function App() {
   return (
-    <div className="max-w-xs bg-white flex-1 font-outfit m-6 p-4 rounded-2xl shadow-sm">
+    <div className="max-w-xs bg-white flex-1 font-outfit m-6 p-4 rounded-2xl shadow-lg">
       <Header />
+      <Main />
     </div>
   );
 }
---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Main() {
+  return (
+    <main>
+      <p className="text-center px-4 text-blue-100 leading-tight mt-4 mb-6">
+        Scan the QR code to visit Frontend Mentor and take your coding skills to
+        the next level
+      </p>
+    </main>
+  );
+}
+
+export default Main;
