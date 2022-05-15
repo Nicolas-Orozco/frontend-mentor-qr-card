468977f57794b02f457bc854d04a08155888c9d5
Nicolas Orozco Davila <nico.orozco.davila@yandex.com>
2022-05-14 21:28:30 -0500

    Add image

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="max-w-xs bg-white flex-1 font-outfit m-6 p-2 rounded-2xl shadow-sm">
+    <div className="max-w-xs bg-white flex-1 font-outfit m-6 p-4 rounded-2xl shadow-sm">
       <Header />
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import Image from "../assets/image-qr-code.png";
 
 function Header() {
   return (
-    <h1 className="text-xl text-center font-bold text-blue-200 px-5">
-      Improve your front-end skills by building projects
-    </h1>
+    <header>
+      <img src={Image} alt="QR code" className="rounded-xl" />
+      <h1 className="text-xl text-center font-bold text-blue-200 px-5 mt-6">
+        Improve your front-end skills by building projects
+      </h1>
+    </header>
   );
 }
 
